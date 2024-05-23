b4e1dc00b2a9b9f9a85a86c5eeb2d822481ee453
Siljupg <siljupg9191@gmail.com>
2024-05-24 11:22:20 +1200

    Added line deleted

--- src/pages/HorrorMovies.js
+++ src/pages/HorrorMovies.js
@@ -17,7 +17,6 @@ function HorrorMoives () {
             </div>
             <div>
             <p className='movie-font'>{movie?.original_title || movie?.original_name}</p>
-            {/* <p>({movie.media_type})</p> */}
             </div>
           </button>
         ))}
